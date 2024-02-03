2c822bfde543442070db8dab7492694213898b84
Nikolai Lønberg <niklonberg@gmail.com>
2024-02-03 13:09:46 +0100

    add Footer component, edit App to import it

--- bro-code-react-course/src/App.tsx
+++ bro-code-react-course/src/App.tsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
