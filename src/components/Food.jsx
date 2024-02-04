efd3ded23a6c83e3c706e64ab62d25d1ac4fc01d
Nikolai Lønberg <niklonberg@gmail.com>
2024-02-04 14:07:14 +0100

    fix folder structure

---
+++ src/components/Food.jsx
@@ -0,0 +1,14 @@
+function Food() {
+  const food1 = "Apple";
+  const food2 = "Banana";
+
+  return (
+    <ul>
+      <li>{food1}</li>
+      <li>{food2}</li>
+      <li>Pear</li>
+    </ul>
+  );
+}
+
+export default Food;
