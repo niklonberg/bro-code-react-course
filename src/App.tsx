04ea7b9c1ab448c607e187fb19a09edfc067d3d3
Nikolai Lønberg <niklonberg@gmail.com>
2024-02-04 14:11:46 +0100

    fix use of .jsx files as components, edit to .tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
